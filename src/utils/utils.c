319f433e5ae3f3efaae631a4246cd0100e8279ae
djeeee1272@gmail.com <djeeee1272@gmail.com>
2021-10-11 15:29:29 +0900

    added is_pipe & is_redirection

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 02:42:40 by jekim             #+#    #+#             */
-/*   Updated: 2021/10/09 21:44:05 by jekim            ###   ########.fr       */
+/*   Updated: 2021/10/11 15:28:50 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,25 @@ int print_intro(int argc, char **argv)
 	printf("%s\n", INTRO);
 	(void)argc;
 	(void)argv;
+	return (0);
+}
+
+int is_pipe(const char *target)
+{
+	if (ft_strncmp(target, "|", 1) == 0)
+		return (1);
+	if (ft_strncmp(target, ">", 1) == 0)
+		return (2);
+	if (ft_strncmp(target, "<", 1) == 0)
+		return (3);
+	return (0);
+}
+
+int is_redirection(const char *target)
+{
+	if (ft_strncmp(target, ">>", 2) == 0)
+		return (1);
+	if (ft_strncmp(target, "<<", 2) == 0)
+		return (2);
 	return (0);
 }
