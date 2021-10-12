0d4f3d513bfeabd76682d8961a7b282c7475def4
chichoon <chichoon.choi@gmail.com>
2021-10-12 13:42:44 +0900

    [minishell] UPDATE: echo

--- src/cmd/echo.c
+++ src/cmd/echo.c
@@ -6,27 +6,39 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:54:11 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/12 11:10:13 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/10/12 13:39:24 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd.h"
 
-
-int	echo_noflag(node *tree, node *envp)
+int	echo_noflag(t_token *tree, t_token *envp)
 {
+	int	len;
 
+	if (tree && tree->contents)
+	{
+		len = ft_strlen(tree->contents);
+		write(1, tree->contents, len);
+	}
+	write(1, "\n", 1);
 }
 
-int	echo_nflag(node *tree, node *envp)
+int	echo_nflag(t_token *tree, t_token *envp)
 {
+	int	len;
 
+	if (tree && tree->contents)
+	{
+		len = ft_strlen(tree->contents);
+		write(1, tree->contents, len);
+	}
 }
 
-int	minishell_echo(node *tree, node *envp)
+int	minishell_echo(t_token *tree, t_token *envp)
 {
-	if (ft_strnstr(tree->next->data, "-n", 2))
-		echo_nflag(tree, envp);
+	if (ft_strnstr(tree->flag, "-n", 2)) //flag 변수 이름 바꿔야
+		echo_nflag(tree->next, envp);
 	else
-		echo_noflag(tree, envp);
+		echo_noflag(tree->next, envp);
 }
