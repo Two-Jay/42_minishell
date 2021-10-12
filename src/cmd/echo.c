8d7876081d79dc834112ef931e147b411a91fb8f
chichoon <chichoon.choi@gmail.com>
2021-10-12 11:10:25 +0900

    [minishell] UPDATE: echo wip

--- src/cmd/echo.c
+++ src/cmd/echo.c
@@ -6,26 +6,27 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:54:11 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/10 19:00:17 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/10/12 11:10:13 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd.h"
 
-int	echo_noflag(node *tree, char *envp[])
+
+int	echo_noflag(node *tree, node *envp)
 {
 
 }
 
-int	echo_nflag(node *tree, char *envp[])
+int	echo_nflag(node *tree, node *envp)
 {
 
 }
 
-int	minishell_echo(node *tree, char *envp[])
+int	minishell_echo(node *tree, node *envp)
 {
 	if (ft_strnstr(tree->next->data, "-n", 2))
-		echo_nflag(tree, flag, envp);
+		echo_nflag(tree, envp);
 	else
-		echo_noflag(str, flag, envp);
+		echo_noflag(tree, envp);
 }
