61bba3838ceb4bf1ff6d8d52d080355286dd9f11
chichoon <chichoon.choi@gmail.com>
2021-10-16 18:12:41 +0900

    [minishell] UPDATE: echo 인자값 update

--- src/cmd/echo.c
+++ src/cmd/echo.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:54:11 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/16 09:31:08 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/10/16 13:54:10 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,23 @@ static int	echo_if_nnn(char *flag)
 	return (1);
 }
 
-static int	echo_noflag(t_token *tree, t_token *envp)
+static int	echo_noflag(t_token *tree)
 {
 	if (tree && tree->contents)
 		ft_putstr_fd(tree->contents, 1);
 	write(1, "\n", 1);
 }
 
-static int	echo_nflag(t_token *tree, t_token *envp)
+static int	echo_nflag(t_token *tree)
 {
 	if (tree && tree->contents)
 		ft_putstr_fd(tree->contents, 1);
 }
 
-int	minishell_echo(t_token *tree, t_token *envp)
+int	minishell_echo(t_data *data)
 {
-	if (echo_if_nnn(tree->flag)) //flag 변수 이름 바꿔야
-		echo_nflag(tree->next, envp);
+	if (echo_if_nnn(data->input->next->contents)) //flag 변수 이름 바꿔야
+		echo_nflag(data->input->next);
 	else
-		echo_noflag(tree->next, envp);
+		echo_noflag(data->input->next);
 }
