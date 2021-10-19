12be083a7c81b0154cdb4b12660265a6e487ec32
chichoon <chichoon.choi@gmail.com>
2021-10-19 13:17:18 +0900

    [minishell] UPDATE: echo가 다음 인자값을 받아오고 free하도록 수정

--- src/cmd/echo.c
+++ src/cmd/echo.c
@@ -6,11 +6,11 @@
 /*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:54:11 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/19 09:35:55 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/10/19 13:16:48 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cmd.h"
+#include "../../includes/cmd2.h"
 
 static int	echo_if_nnn(char *flag)
 {
@@ -28,15 +28,41 @@ static int	echo_if_nnn(char *flag)
 
 static int	echo_noflag(t_token *tree)
 {
-	if (tree && tree->content)
-		ft_putstr_fd(tree->content, 1);
+	char	**str_to_print;
+	int		i;
+
+	while (tree)
+	{
+		str_to_print = trim_quote_and_parse(tree->content);
+		if (!str_to_print)
+			return (-1);
+		i = -1;
+		while (str_to_print[++i])
+			ft_putstr_fd(str_to_print[i], 1);
+		ft_free_char2d(str_to_print);
+		tree = tree->next;
+	}
 	write(1, "\n", 1);
+	return (0);
 }
 
 static int	echo_nflag(t_token *tree)
 {
-	if (tree && tree->content)
-		ft_putstr_fd(tree->content, 1);
+	char	**str_to_print;
+	int		i;
+
+	while (tree)
+	{
+		str_to_print = trim_quote_and_parse(tree->content);
+		if (!str_to_print)
+			return (-1);
+		i = -1;
+		while (str_to_print[++i])
+			ft_putstr_fd(str_to_print[i], 1);
+		ft_free_char2d(str_to_print);
+		tree = tree->next;
+	}
+	return (0);
 }
 
 int	minishell_echo(t_data *data)
