40f78a67f9beda9c64fe55dcb97181f677c574ee
chichoon <chichoon.choi@gmail.com>
2021-10-20 22:23:04 +0900

    [minishell] UPDATE: exit 완성

--- src/cmd/exit.c
+++ src/cmd/exit.c
@@ -6,13 +6,13 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 21:24:06 by jekim             #+#    #+#             */
-/*   Updated: 2021/10/20 14:18:43 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/10/20 22:22:48 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-long long	is_overflow_ll(char *str)
+static long long	is_overflow_ll(char *str)
 {
 	unsigned long long	converted;
 	int					minus;
@@ -32,6 +32,8 @@ long long	is_overflow_ll(char *str)
 		return (-1);
 	if (converted - 1 > LLMAX && minus == -1)
 		return (-1);
+	if (minus == -1)
+		return (256 - (converted % 256));
 	return (converted % 256);
 }
 
@@ -48,17 +50,19 @@ static int	is_num(char *str)
 	return (1);
 }
 
-void	exit_with_param(t_data *data)
+static void	exit_with_param(t_data *data)
 {
 	char		*str;
 	long long	errno_converted;
 
 	str = data->input->next->content;
 	errno_converted = 0;
+	write(1, "exit\n", 5);
 	if (!is_num(str) || (ft_strlen(str) > 20)
 		|| (ft_strlen(str) > 19 && *str != '-'))
 	{
 		builtin_error(data, str, EXIT_ERRNUM, 255);
+		// $? 세팅: 255 (buitin_error 내부에서)
 		exit(255);
 	}
 	else
@@ -67,16 +71,18 @@ void	exit_with_param(t_data *data)
 		if (errno_converted < 0)
 		{
 			builtin_error(data, str, EXIT_ERRNUM, 255);
+			//$? 세팅: 255 (buitin_error 내부에서)
 			exit(255);
 		}
-		else
-			write(1, "exit\n", 5);
 	}
+	//$? 세팅: errno_converted
 	exit(errno_converted);
 }
 
-void	exit_no_param(void)
+static void	exit_no_param(void)
 {
+	//$? 세팅: 0
+	write(1, "exit\n", 5);
 	exit(0);
 }
 
