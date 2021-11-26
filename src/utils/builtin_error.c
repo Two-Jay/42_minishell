a06a19b4837129191116c5554e298f78ccb7dc6e
chichoon <chichoon.choi@gmail.com>
2021-11-27 01:05:05 +0900

    [minishell] UPDATE: 빌트인 에러 함수 수정

--- src/utils/builtin_error.c
+++ src/utils/builtin_error.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_error.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jekim <jekim@42seoul.student.com>          +#+  +:+       +#+        */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 14:09:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/11/23 15:17:35 by jekim            ###   ########.fr       */
+/*   Updated: 2021/11/27 01:01:24 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	builtin_error(t_data *data, char *value_str, char *error_str, int dollar_q)
+int	builtin_error(t_data *data, char *cmd, char *value_str, char *error_str)
 {
-	(void)dollar_q;
-	ft_putstr_fd(data->input->content, 2);
+	//(void)dollar_q;
+	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
 	if (value_str)
 		ft_putstr_fd(value_str, 2);
