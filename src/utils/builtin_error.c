dce1db9262d72e40bc4c5e9c1d23fa472ae5eb1a
chichoon <chichoon.choi@gmail.com>
2021-11-27 11:08:30 +0900

    [minishell] UPDATE: builtin error 출력함수

--- src/utils/builtin_error.c
+++ src/utils/builtin_error.c
@@ -6,21 +6,20 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 14:09:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/11/27 01:01:24 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/11/27 10:39:06 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	builtin_error(t_data *data, char *cmd, char *value_str, char *error_str)
+int	builtin_error(t_data *data, char *cmd, char *error_str, int dollar_q)
 {
-	//(void)dollar_q;
+	(void)dollar_q;
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
-	if (value_str)
-		ft_putstr_fd(value_str, 2);
 	ft_putstr_fd(error_str, 2);
 	write(2, "\n", 1);
 	// dq = dollar_q ($? 세팅)
-	return (-1);
+	free(error_str);
+	return (free_token(data->input, -1));
 }
