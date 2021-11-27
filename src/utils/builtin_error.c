b71c464ca594d7efeadcfde325ce4bc25da48640
chichoon <chichoon.choi@gmail.com>
2021-11-27 22:46:58 +0900

    [minishell] UPDATE: 빌트인 에러출력 함수에서 메모리해제 안하도록 변경

--- src/utils/builtin_error.c
+++ src/utils/builtin_error.c
@@ -6,13 +6,13 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 14:09:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/11/27 11:11:02 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/11/27 21:57:22 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	builtin_error(t_data *data, char *cmd, char *error_str, int dollar_q)
+int	builtin_error(char *cmd, char *error_str, int dollar_q)
 {
 	(void)dollar_q;
 	ft_putstr_fd(cmd, 2);
@@ -21,5 +21,5 @@ int	builtin_error(t_data *data, char *cmd, char *error_str, int dollar_q)
 	write(2, "\n", 1);
 	// dq = dollar_q ($? 세팅)
 	free(error_str);
-	return (free_token(data->input, -1));
+	return (-1);
 }
