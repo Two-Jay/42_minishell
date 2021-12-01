5243b45f17ab7f14a23ab4a90fa03ba791922529
chichoon <chichoon.choi@gmail.com>
2021-12-01 21:15:40 +0900

    [minishell] UPDATE: 빌트인 외 프로그램 실행시 에러메시지 중복 해결

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 18:39:47 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/01 17:23:30 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/01 21:12:49 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,18 +56,17 @@ int	exec_program(t_token *input, char *envp[])
 	char	*cmd_path;
 	char	**exec_argv;
 
-	cmd_path = getcmd(input->content, envp);
+	cmd_path = exec_getcmd(input->content, envp);
 	if (!cmd_path)
-		exit(builtin_error(
-				"shell", ft_strjoin(input->content, EXEC_ERRNOCMD), 127));
+		exit(127); //$? 반환해야함
 	exec_argv = pipe_insert_arr(input, cmd_path);
 	if (!exec_argv)
-		exit(builtin_error(
+		exit(child_error(
 				"shell", ft_strjoin(input->content, EXEC_ERRPARSE), 1));
 	execve(cmd_path, exec_argv, envp);
 	ft_free_char2d(exec_argv);
 	free(cmd_path);
-	exit(builtin_error("pipe", ft_strdup(PIPE_ERR), 1));
+	exit(child_error("pipe", ft_strdup(PIPE_ERR), 1));
 }
 
 int	executor(t_data *data, char *envp[])
