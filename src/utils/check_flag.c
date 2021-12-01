89224e2d7593bdc9614cd196dc75ab1c7cb5bf77
chichoon <chichoon.choi@gmail.com>
2021-12-02 01:14:42 +0900

    [minishell] UPDATE: flag 체크 함수 수정

--- src/utils/check_flag.c
+++ src/utils/check_flag.c
@@ -6,20 +6,17 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 13:06:34 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/20 13:39:20 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/01 21:56:45 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	check_flag(t_data *data)
+int	check_flag(t_token *input)
 {
-	char	*str;
-
-	if (!data->input->next)
+	if (!input->next)
 		return (0);
-	str = data->input->next->content;
-	if (*str == '-' && ft_isalpha(*(str + 1)))
+	if (input->next->type == FLAG)
 		return (1);
 	return (0);
 }
