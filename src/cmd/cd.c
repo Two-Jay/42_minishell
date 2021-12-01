f13aaed884b50e2c164d0cbba7a732fbd294cb5e
chichoon <chichoon.choi@gmail.com>
2021-12-02 01:16:25 +0900

    [minishell] UPDATE: cd 함수 wip

--- src/cmd/cd.c
+++ src/cmd/cd.c
@@ -6,16 +6,46 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:50:41 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/11/27 22:48:45 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/02 00:48:22 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	minishell_cd(t_data *data, )
+int	cd_move_directory(t_data *data)
 {
-	int	return_value;
+	char	*str;
 
-	if (check_flag(data))
-		return (builtin_error())
+	str = data->input->next;
+	if (ft_strequel(str, "~"))
+		if (chdir(find_env("HOME", data)) < 0)
+			return (-1);
+	else if (ft_strequel(str, "-"))
+		if (chdir(find_env("OLDPWD", data)) < 0)
+			return (-1);
+	else
+		if (chdir(str) < 0)
+			return (-1);
+	return (0);
+}
+
+int	minishell_cd(t_data *data)
+{
+	t_token	*input;
+
+	input = data->input;
+	if (!input->next)
+		return (0);
+	if (check_flag(input))
+		return (builtin_error("cd",
+				ft_strjoin(input->next->content, CD_ERROPT), 1));
+	if (input->next->type == PIPE || input->next->type == REDIRECT)
+		return (0);
+	if (cd_move_directory(data) < 0)
+		return (builtin_error("cd",
+				ft_strjoin(input->next->content, CD_ERRNODIR), 1));
+	if (cd_add_env(data) < 0)
+		return (builtin_error("cd",
+				ft_strdup(strerror(errno)), 1));
+	return (0);
 }
