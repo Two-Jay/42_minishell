c81fea5cee145493f3f54a3d519bcf84486e5497
chichoon <chichoon.choi@gmail.com>
2021-12-02 15:48:06 +0900

    [minishell] UPDATE: cd 일단 끝 (테스트 해봐야함)

--- src/cmd/cd.c
+++ src/cmd/cd.c
@@ -6,13 +6,13 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:50:41 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/02 14:40:28 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:47:50 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
 
-int	cd_move_directory(t_data *data)
+static int	cd_move_directory(t_data *data)
 {
 	char	*str;
 
@@ -27,6 +27,28 @@ int	cd_move_directory(t_data *data)
 		if (chdir(str) < 0)
 			return (CD_FAILED);
 	return (CD_SUCCESS);
+}
+
+static int	cd_add_pwd(t_data *data)
+{
+	t_envlst	*node_oldpwd;
+	char		*env_old_value;
+	char		*env_value;
+
+	node_oldpwd = find_env("OLDPWD", data);
+	env_old_value = get_env("PWD", data);
+	env_value = getcwd(0, 10);
+	if (!env_value)
+	{
+		free(env_old_value);
+		return (-1);
+	}
+	if (!env_old_value)
+		save_env(data, ft_strdup("PWD"), env_value, ENV);
+	env_old_value = get_env("PWD", data);
+	if (!node_oldpwd)
+		save_env(data, ft_strdup("OLDPWD"), env_old_value, ENV);
+	return (save_env(data, ft_strdup("PWD"), env_value, ENV));
 }
 
 int	minishell_cd(t_data *data)
@@ -48,7 +70,7 @@ int	minishell_cd(t_data *data)
 	if (cd_move_directory(data) == CD_FAILED)
 		return (builtin_error("cd",
 				ft_strjoin(input->next->content, CD_ERRNODIR), 1));
-	if (cd_add_env(data) < 0)
+	if (cd_add_pwd(data) < 0)
 		return (builtin_error("cd",
 				ft_strdup(strerror(errno)), 1));
 	return (0);
