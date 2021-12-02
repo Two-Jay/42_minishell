496c85b9d0793cf4440f3f5cac2bfffe3b7c560a
chichoon <chichoon.choi@gmail.com>
2021-12-02 14:55:37 +0900

    [minishell] UPDATE: cd wip (pwd 환경변수 추가부분 만드는중)

--- src/cmd/cd.c
+++ src/cmd/cd.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:50:41 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/02 00:48:22 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/02 14:40:28 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	cd_move_directory(t_data *data)
 	str = data->input->next;
 	if (ft_strequel(str, "~"))
 		if (chdir(find_env("HOME", data)) < 0)
-			return (-1);
+			return (CD_HOMENOTSET);
 	else if (ft_strequel(str, "-"))
 		if (chdir(find_env("OLDPWD", data)) < 0)
-			return (-1);
+			return (CD_PWDNOTSET);
 	else
 		if (chdir(str) < 0)
-			return (-1);
-	return (0);
+			return (CD_FAILED);
+	return (CD_SUCCESS);
 }
 
 int	minishell_cd(t_data *data)
@@ -41,7 +41,11 @@ int	minishell_cd(t_data *data)
 				ft_strjoin(input->next->content, CD_ERROPT), 1));
 	if (input->next->type == PIPE || input->next->type == REDIRECT)
 		return (0);
-	if (cd_move_directory(data) < 0)
+	if (cd_move_directory(data) == CD_PWDNOTSET)
+		return (builtin_error("cd", ft_strdup(CD_ERROLD), 1));
+	if (cd_move_directory(data) == CD_HOMENOTSET)
+		return (builtin_error("cd", ft_strdup(CD_ERRHOME), 1));
+	if (cd_move_directory(data) == CD_FAILED)
 		return (builtin_error("cd",
 				ft_strjoin(input->next->content, CD_ERRNODIR), 1));
 	if (cd_add_env(data) < 0)
