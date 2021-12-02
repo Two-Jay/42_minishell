37d1af18232e37c5596ede605bee2cc012bbf167
chichoon <chichoon.choi@gmail.com>
2021-12-02 15:11:17 +0900

    [minishell] UPDATE: save_env 개편 (기존 노드 발견 여부로 분할)

--- src/utils/save_env.c
+++ src/utils/save_env.c
@@ -6,11 +6,63 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 15:00:59 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/11/27 15:03:48 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:10:43 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
+
+static t_envlst	*get_last_node(t_data *data)
+{
+	t_envlst	*node_found;
+
+	node_found = data->envlst;
+	if (!node_found)
+		return (NULL);
+	while (node_found->next)
+		node_found = node_found->next;
+	return (node_found);
+}
+
+static int	save_new_env(
+	t_data *data, char *env_key, char *env_value, t_env_state flag)
+{
+	t_envlst	*node_last;
+	t_envlst	*node_new;
+
+	node_new = malloc(sizeof(t_envlst));
+	if (!node_new)
+		return (-1);
+	node_new->key = env_key;
+	node_new->value = env_value;
+	node_new->env_state = flag;
+	node_new->next = NULL;
+	node_last = get_last_node(data);
+	if (!node_last)
+	{
+		free(env_key);
+		free(env_value);
+		free(node_new);
+		return (-1);
+	}
+	node_last->next = node_new;
+	return (0);
+}
+
+static void	override_env(
+	t_envlst *node_found, char *env_key, char *env_value)
+{
+	if (node_found->key)
+	{
+		free(node_found->key);
+		node_found->key = env_key;
+	}
+	if (node_found->value)
+	{
+		free(node_found->value);
+		node_found->value = env_value;
+	}
+}
 
 int	save_env(
 	t_data *data, char *env_key, char *env_value, t_env_state flag)
@@ -19,25 +71,8 @@ int	save_env(
 	t_envlst	*node_new;
 
 	node_found = find_env(env_key, data);
-	node_new = NULL;
 	if (!node_found)
-	{
-		node_found = data->envlst;
-		while (node_found->next != NULL)
-			node_found = node_found->next;
-		node_new = malloc(sizeof(t_envlst));
-		if (!node_new)
-			return (-1);
-		node_new->key = env_key;
-		node_new->value = env_value;
-		node_new->env_state = flag;
-		node_new->next = NULL;
-		node_found->next = node_new;
-		return (0);
-	}
-	if (node_new->value)
-		free(node_new->value);
-	free(env_key);
-	node_new->value = env_value;
+		return (save_new_env(data, env_key, env_value, flag));
+	override_env(node_found, env_key, env_value);
 	return (0);
 }
