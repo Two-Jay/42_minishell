7aa4cb8e931491b090cc8488e609b88592cec34f
chichoon <chichoon.choi@gmail.com>
2021-12-02 17:02:57 +0900

    [minishell[ UPDATE: builtin_error 예외처리

--- src/utils/builtin_error.c
+++ src/utils/builtin_error.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 14:09:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/01 21:04:25 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/02 17:02:36 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	builtin_error(char *cmd, char *error_str, int dollar_q)
 	ft_putstr_fd(error_str, 2);
 	write(2, "\n", 1);
 	// dq = dollar_q ($? 세팅)
-	free(error_str);
+	if (error_str)
+		free(error_str);
 	return (-1);
 }
 
