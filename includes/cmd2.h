252e03c7223940340f886f6b8a5d0765cff5dfda
chichoon <chichoon.choi@gmail.com>
2021-12-04 13:09:14 +0900

    [minishell] UPDATE: pwd 선언 인자 수정

--- includes/cmd2.h
+++ includes/cmd2.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 11:40:00 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/04 12:38:35 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/04 13:07:30 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,7 @@ char		**pipe_insert_arr(t_token *input, char *cmd_path);
 # define	CD_FAILED -1
 # define	CD_SUCCESS 0
 int			minishell_cd(t_data *data, t_token *input);
-int			minishell_pwd(t_data *data, t_token *input);
-
+int			minishell_pwd(t_token *input);
 /*
 	* echo
 */
