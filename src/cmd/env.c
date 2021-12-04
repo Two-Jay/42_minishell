4b349281ea6e2d0d1aa553e7a8a7800e7e4b3ea2
chichoon <chichoon.choi@gmail.com>
2021-12-04 14:27:34 +0900

    [minishell] UPDATE: env 끝, 누수없음

--- src/cmd/env.c
+++ src/cmd/env.c
@@ -6,8 +6,40 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 20:26:51 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/04 12:41:13 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/04 14:25:29 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cmd2.h"
+
+static int	env_no_param(t_data *data, t_token *input)
+{
+	t_envlst	*node_tmp;
+	int			fd;
+
+	node_tmp = data->envlst;
+	fd = get_redir_fd(input);
+	while (node_tmp)
+	{
+		if (node_tmp->env_state == ENV)
+		{
+			ft_putstr_fd(node_tmp->key, fd);
+			ft_putstr_fd("=", fd);
+			ft_putstr_fd(node_tmp->value, fd);
+			ft_putstr_fd("\n", fd);
+		}
+		node_tmp = node_tmp->next;
+	}
+	if (fd != STDOUT_FILENO)
+		close(fd);
+	return (0);
+}
+
+int	minishell_env(t_data *data, t_token *input)
+{
+	if (input->next
+		&& (input->next->type != REDIRECT && input->next->type != PIPE))
+		return (builtin_error("env",
+				ft_strjoin(input->next->content, ENV_ERROPT), 1));
+	return (env_no_param(data, input->next));
+}
---
+++ src/test/test_env.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 14:06:28 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/12/04 14:26:42 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cmd2.h"
+
+int	main(void)
+{
+	t_data		*data;
+	t_envlst	*env[4];
+	t_token		*input[3];
+
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (0);
+	for (int i = 0; i < 4; i++)
+	{
+		env[i] = malloc(sizeof(t_envlst));
+		if (!env[i])
+			return (0);
+		env[i]->env_state = ENV;
+		if (i != 0)
+			env[i - 1]->next = env[i];
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		input[i] = malloc(sizeof(t_token));
+		if (!input[i])
+			return (0);
+		input[i]->ix = i;
+		if (i != 0)
+		{
+			input[i - 1]->next = input[i];
+			input[i]->prev = input[i - 1];
+		}
+	}
+	env[0]->key = ft_strdup("a");
+	env[0]->value = ft_strdup("10");
+	env[1]->key = ft_strdup("b");
+	env[1]->value = ft_strdup("10");
+	env[2]->key = ft_strdup("c");
+	env[2]->value = ft_strdup("10");
+	env[3]->key = ft_strdup("d");
+	env[3]->value = ft_strdup("10");
+	env[3]->next = NULL;
+	input[0]->prev = NULL;
+	input[2]->next = NULL;
+	input[0]->content = ft_strdup("env");
+	input[0]->type = CMD;
+	input[1]->content = ft_strdup(">>");
+	input[1]->type = REDIRECT;
+	input[2]->content = ft_strdup("test.txt");
+	input[2]->type = FILEPATH;
+	data->envlst = env[0];
+	data->input = input[0];
+	minishell_env(data, data->input);
+	for (int i = 0; i < 4; i++)
+	{
+		free(env[i]->key);
+		free(env[i]->value);
+		free(env[i]);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		free(input[i]->content);
+		free(input[i]);
+	}
+	free(data);
+	system("leaks a.out");
+}
