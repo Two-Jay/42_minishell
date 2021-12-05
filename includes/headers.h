3df338ab8064a2fe57b8dfac4e56626296f8ef8a
chichoon <chichoon.choi@gmail.com>
2021-12-05 17:06:18 +0900

    [minishell] ADD: 구조체 헤더, 헤더 Import 헤더 추가

---
+++ includes/headers.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   headers.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 17:04:00 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/12/05 17:04:31 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADERS_H
+# define HEADERS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <dirent.h>
+# include <signal.h>
+# include <termios.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../libft/libft.h"
+
+#endif
