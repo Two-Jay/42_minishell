258b1f39d3273cbbd4208190ad0a80dde8ba8210
chichoon <chichoon.choi@gmail.com>
2021-12-05 17:30:39 +0900

    [minishell] UPDATE: 구조체 순서 변경

--- includes/structure.h
+++ includes/structure.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 17:02:25 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/05 17:03:13 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/05 17:09:57 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@
 /*
 	* Structures
 */
+typedef struct s_input_process
+{
+	char	*scenv_ret;
+	int		*qtrim_checker;
+	char	*isbs_ret;
+	char	**split_ret;
+}	t_input_process;
+
+typedef struct s_parser_env_bucket
+{
+	char	*srcp;
+	int		now_ix;
+	int		src_l;
+	char	*envkey;
+	int		key_l;
+	char	*envval;
+	int		value_l;
+	char	*errno_str;
+	int		errno_l;
+}	t_eb;
+
 typedef enum s_state
 {
 	CMD = 0,
@@ -68,26 +89,5 @@ typedef struct s_pipe
 	int		fd_tmp;
 	int		last_pid;
 }	t_pipe;
-
-typedef struct input_process
-{
-	char	*scenv_ret;
-	int		*qtrim_checker;
-	char	*isbs_ret;
-	char	**split_ret;
-}	t_input_process;
-
-typedef struct s_parser_env_bucket
-{
-	char	*srcp;
-	int		now_ix;
-	int		src_l;
-	char	*envkey;
-	int		key_l;
-	char	*envval;
-	int		value_l;
-	char	*errno_str;
-	int		errno_l;
-}	t_eb;
 
 #endif
