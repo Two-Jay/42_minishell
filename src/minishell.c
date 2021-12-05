6c5d5fe1df193e8126eb7dc8c00f76236eb28db3
chichoon <chichoon.choi@gmail.com>
2021-12-05 17:30:50 +0900

    [minishell] UPDATE: minishell.c norminette

--- src/minishell.c
+++ src/minishell.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jekim <jekim@42seoul.student.com>          +#+  +:+       +#+        */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 14:08:41 by jekim             #+#    #+#             */
-/*   Updated: 2021/11/23 14:44:51 by jekim            ###   ########.fr       */
+/*   Updated: 2021/12/05 17:09:07 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
 	t_data	data;
