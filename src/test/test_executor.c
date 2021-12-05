0f6f7b6a9c376e46f7e9456daa1d3f43cea815d3
chichoon <chichoon.choi@gmail.com>
2021-12-05 18:51:28 +0900

    [minishell] UPDATE: 좌측 리디렉션 (<) 체크

--- src/test/test_executor.c
+++ src/test/test_executor.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 17:41:01 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/05 18:36:58 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/05 18:50:42 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,26 +47,26 @@ int	main(int argc, char *argv[], char *envp[])
 	input[9]->next = NULL;
 	input[0]->prev = NULL;
 	//test
-	input[0]->content = ft_strdup("echo");
+	input[0]->content = ft_strdup("cat");
 	input[0]->type = CMD;
-	input[1]->content = ft_strdup("hello");
-	input[1]->type = STR;
-	input[2]->content = ft_strdup("|");
-	input[2]->type = PIPE;
-	input[3]->content = ft_strdup("cat");
-	input[3]->type = CMD;
-	input[4]->content = ft_strdup("-e");
-	input[4]->type = FLAG;
-	input[5]->content = ft_strdup("|");
-	input[5]->type = PIPE;
-	input[6]->content = ft_strdup("cat");
-	input[6]->type = CMD;
-	input[7]->content = ft_strdup("-e");
-	input[7]->type = FLAG;
-	input[8]->content = ft_strdup("|");
-	input[8]->type = PIPE;
-	input[9]->content = ft_strdup("cat");
-	input[9]->type = CMD;
+	input[1]->content = ft_strdup("-e");
+	input[1]->type = FLAG;
+	input[2]->content = ft_strdup("<");
+	input[2]->type = REDIRECT;
+	input[3]->content = ft_strdup("test.txt");
+	input[3]->type = FILEPATH;
+	input[4]->content = ft_strdup("|");
+	input[4]->type = PIPE;
+	input[5]->content = ft_strdup("cat");
+	input[5]->type = CMD;
+	input[6]->content = ft_strdup("-e");
+	input[6]->type = FLAG;
+	input[7]->content = ft_strdup("|");
+	input[7]->type = PIPE;
+	input[8]->content = ft_strdup("cat");
+	input[8]->type = CMD;
+	input[9]->content = ft_strdup("-e");
+	input[9]->type = FLAG;
 	data->input = input[0];
 	data->envlst = env1;
 	minishell_executor(data, envp);
