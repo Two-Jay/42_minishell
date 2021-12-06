2ac27f0f352ed7127b2e8fba4bad977f1ae14fce
chichoon <chichoon.choi@gmail.com>
2021-12-07 02:01:50 +0900

    [minishell] UPDATE: exit 수정

--- includes/cmd.h
+++ includes/cmd.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 11:40:00 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/07 01:04:15 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/07 01:54:15 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,7 @@ int			minishell_unset(t_data *data, t_token *input);
 */
 # define	EXIT_ERRNUM ": numeric argument required"
 # define	EXIT_ERRMANY "too many arguments"
-int			minishell_exit(t_data *data, t_token *input);
+int			minishell_exit(t_token *input);
 
 /*
 	* Redirection
@@ -128,6 +128,7 @@ int			if_builtin(t_token *input);
 int			check_flag(t_token *input);
 t_token		*find_flag(t_token *input);
 int			check_argument(t_token *input);
+int			check_arg_num(t_token *input);
 int			free_token(t_token *input);
 void		ft_free_char2d(char **arr);
 
--- src/utils/check_argument.c
+++ src/utils/check_argument.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 00:44:49 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/07 00:59:40 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/07 01:58:04 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,21 @@ int	check_argument(t_token *input)
 	if (!input || input->type == PIPE)
 		return (ARGUMENT_X);
 	return (ARGUMENT_O);
+}
+
+int	check_arg_num(t_token *input)
+{
+	int	n;
+
+	n = 0;
+	if (!input->next || input->next->type == PIPE)
+		return (n);
+	input = input->next;
+	while (input && input->type != PIPE)
+	{
+		if (input->type == STR || input->type == FLAG)
+			n++;
+		input = input->next;
+	}
+	return (n);
 }
