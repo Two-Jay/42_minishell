a1f74584237f12c4b808d8966e65897d983d1b5d
djeeee1272@gmail.com <djeeee1272@gmail.com>
2021-12-06 14:12:26 +0900

    UODATE : refactored ft_strjoin to handle nullish string bug

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 05:39:20 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/28 03:04:45 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/12/06 14:10:33 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,41 @@ static char	*set_memory(const char *s1, const char *s2,
 	return (ret);
 }
 
+static char *handle_nullish_input(const char *s1, const char *s2)
+{
+	if (!(s1))
+		return (ft_strdup(s2));
+	if (!(s1))
+		return (ft_strdup(s1));
+	return (NULL);
+}
+
+static void join_string(char *ret, const char *s1, const char *s2)
+{
+	int ix;
+	int jx;
+
+	ix = 0;
+	jx = 0;
+	while (s1[ix])
+		ret[jx++] = s1[ix++];
+	ix = 0;
+	while (s2[ix])
+		ret[jx++] = s2[ix++];	
+}
+
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char			*ret;
 	unsigned int	s1_l;
 	unsigned int	s2_l;
 
-	if (!s1 && !s2)
-		return (NULL);
-	else if (!s1 || !s2)
-	{
-		if (!(s1))
-			return (ft_strdup(s2));
-		else
-			return (ft_strdup(s1));
-	}
+	if ((!s1 && !s2) || (!s1 || !s2))
+		return (handle_nullish_input(s1, s2));
 	ret = set_memory(s1, s2, &s1_l, &s2_l);
 	if (!ret)
 		return (NULL);
-	while (*s1)
-		*(ret++) = *(s1++);
-	while (*s2)
-		*(ret++) = *(s2++);
-	*ret = '\0';
+	join_string(ret, s1, s2); 
+	ret[s1_l + s2_l] = '\0';
 	return (ret);
 }
