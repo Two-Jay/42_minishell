1f5f1e068f8cdfaa2380309816da65c59325ad07
chichoon <chichoon.choi@gmail.com>
2021-12-07 19:59:58 +0900

    [minishell] UPDATE: free_token 릭안나게 수정

--- src/utils/free_token.c
+++ src/utils/free_token.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 00:56:40 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/06 22:05:50 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/07 19:57:37 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 int	free_token(t_token *input)
 {
-	t_token	*tree;
+	t_token	*token_todel;
+	t_token	*token_temp;
 
-	tree = input->next;
-	while (tree && tree->next)
+	token_temp = input->next;
+	while (token_temp->next)
 	{
-		if (tree->prev->content)
-			free(tree->prev->content);
-		free(tree->prev);
-		tree = tree->next;
+		token_todel = token_temp->prev;
+		if (token_todel->content)
+			free(token_todel->content);
+		free(token_todel);
+		token_temp = token_temp->next;
 	}
-	free(tree->content);
-	free(tree);
+	token_todel = token_temp->prev;
+	if (token_todel->content)
+		free(token_todel->content);
+	free(token_todel);
+	free(token_temp->content);
+	free(token_temp);
 	return (0);
 }
