b09186cdd7d7f091a6e475fa8b3d7aacc15d86a4
chichoon <chichoon.choi@gmail.com>
2021-12-15 22:16:22 +0900

    [minishell] UPDATE: init_signal 함수 헤더에 추가

--- includes/signals.h
+++ includes/signals.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:00:36 by jekim             #+#    #+#             */
-/*   Updated: 2021/12/15 17:53:41 by jekim            ###   ########.fr       */
+/*   Updated: 2021/12/15 21:34:17 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	set_signal_handler_ignore(int signo);
 void	signal_handler_default_SIGINT(int signo);
 void	signal_handler_blocked_cmd(int signo);
 void	signal_handler_interrupt_heredoc(int signo);
+void	init_signal(int signo);
 
 #endif
