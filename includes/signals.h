1cba8d4346628323b87c8eb2ac33da6d3d7a8084
djeeee1272@gmail.com <djeeee1272@gmail.com>
2021-12-18 18:47:07 +0900

    UPDATE : added termattr prototypes in signals.h

--- includes/signals.h
+++ includes/signals.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:00:36 by jekim             #+#    #+#             */
-/*   Updated: 2021/12/17 12:04:01 by jekim            ###   ########.fr       */
+/*   Updated: 2021/12/18 18:46:31 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ void	signal_handler_default_SIGINT(int signo);
 void	signal_handler_blocked_cmd(int signo);
 void	signal_handler_interrupt_heredoc(int signo);
 void	init_signal(int signo);
+
+void backup_termattr(struct termios *termattr);
+void reset_termattr(struct termios *termattr);
+void turnoff_echoctl_termattr(void);
 
 #endif
