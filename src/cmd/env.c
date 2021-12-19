690bb0018065a65b19ab44a9eaff208ae9f62d35
chichoon <chichoon.choi@gmail.com>
2021-12-19 15:17:27 +0900

    [minishell] UPDATE: env flag, argument 이슈 수정

--- src/cmd/env.c
+++ src/cmd/env.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 20:26:51 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/12/19 15:05:45 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/19 15:09:56 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,12 @@ int	minishell_env(t_data *data, t_token *input, int ofd)
 
 	argument_check = check_argument(input);
 	if (argument_check == FLAG_O)
-	{
-		input = input->next;
-		while (input && (input->type == REDIRECT || input->type == FILEPATH))
-			input = input->next;
 		return (builtin_error("env",
-				ft_strjoin(input->content, ENV_ERROPT), 1));
-	}
+				ft_strjoin(find_flag(input)->content, ENV_ERROPT), 1));
 	else if (argument_check == ARGUMENT_O)
 	{
-		input = input->next;
-		while (input && (input->type == REDIRECT || input->type == FILEPATH))
-			input = input->next;
 		return (builtin_error("env",
-				ft_strjoin(input->content, ENV_ERRNODIR), 1));
+				ft_strjoin(find_arg(input)->content, ENV_ERRNODIR), 1));
 	}
 	return (env_no_param(data, ofd));
 }
