baa10ea8b0dcff54050b5f4185b3c82fdfe10dd0
chichoon <chichoon.choi@gmail.com>
2021-12-19 23:10:26 +0900

    [minishell] UPDATE: 함수 무한루프 수정

--- src/parser/syntax_error_guard.c
+++ src/parser/syntax_error_guard.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 18:56:16 by jekim             #+#    #+#             */
-/*   Updated: 2021/12/19 22:42:35 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/19 22:51:15 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,26 +66,23 @@ int	check_no_cmd_between_pipes(t_token *token)
 {
 	t_token	*tknp;
 	int		cnt_cmd;
-	int		err_flag;
 
 	cnt_cmd = 0;
-	err_flag = 0;
 	tknp = token;
+	if (!tknp)
+		return (0);
 	while (tknp->next)
 	{
-		printf("token: %s\n", tknp->content);
 		if (cnt_cmd == 0 && tknp->type == PIPE)
-		{
-			err_flag = 1;
-			break ;
-		}
+			return (print_syntax_error(PSR_ERRHEAD,
+					ft_strjoin(PSR_ERRSYNTAX, tknp->content)));
 		if (tknp->type == PIPE)
 			cnt_cmd = 0;
 		if (tknp->type == CMD)
 			cnt_cmd++;
 		tknp = tknp->next;
 	}
-	if (tknp->type == PIPE || err_flag)
+	if (tknp->type == PIPE)
 		return (print_syntax_error(PSR_ERRHEAD,
 				ft_strjoin(PSR_ERRSYNTAX, tknp->content)));
 	return (0);
